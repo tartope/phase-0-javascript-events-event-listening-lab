917ab1a693e2ec87b511bc20e0fc2a020c4766f8
Tunisia Artope <artope@yahoo.com>
2022-02-08 11:17:48 -0500

    commit tree 39caaf8e641fc899150e7c03329ec8cafd4b755f

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-
-function clickAlert() {
-  alert('LOL, that tickles!');
-}
-
+function addingEventListener(){
+const input = document.getElementById('input');
 input.addEventListener('click', clickAlert);
 }
 
-addingEventListener();
+function clickAlert(){
+  alert ('I was clicked.');
+}
+
+
+
+
+
+
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('LOL, that tickles!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+
+// addingEventListener();
